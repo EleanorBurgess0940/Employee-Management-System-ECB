5bf344f487487ba2512d35c76996cda7886644af
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-05-12 21:08:46 -0400

    functionality of the view Employees was added

--- index.js
+++ index.js
@@ -19,54 +19,54 @@ async function mainMenu() {
           name: "View All Employees",
           value: "viewEmployees",
         },
-        {
-          name: "View All Employees By Department",
-          value: "viewEmployeesDepartment",
-        },
-        {
-          name: "View All Employees By Manager",
-          value: "viewEmployeesManager",
-        },
-        {
-          name: "Add Employee",
-          value: "addEmployee",
-        },
-        {
-          name: "Remove Employee",
-          value: "removeEmployee",
-        },
-        {
-          name: "Update Employee Role",
-          value: "updateEmployeeRole",
-        },
-        {
-          name: "Update Employee Manager",
-          value: "updateEmployeeManager",
-        },
-        {
-          name: "View All Roles",
-          value: "viewAllRoles",
-        },
-        {
-          name: "Add Role",
-          value: "addRole",
-        },
-        {
-          name: "Remove Role",
-          value: "removeRole",
-        },
-        {
-          name: "View All Departments",
-          value: "viewAllDepartments",
-        },
-        {
-          name: "Add Department",
-          value: "addDepartment",
-        },
-        {
-          name: "Remove Department",
-          value: "removeDepartment",
-        },
+        // {
+        //   name: "View All Employees By Department",
+        //   value: "viewEmployeesDepartment",
+        // },
+        // {
+        //   name: "View All Employees By Manager",
+        //   value: "viewEmployeesManager",
+        // },
+        // {
+        //   name: "Add Employee",
+        //   value: "addEmployee",
+        // },
+        // {
+        //   name: "Remove Employee",
+        //   value: "removeEmployee",
+        // },
+        // {
+        //   name: "Update Employee Role",
+        //   value: "updateEmployeeRole",
+        // },
+        // {
+        //   name: "Update Employee Manager",
+        //   value: "updateEmployeeManager",
+        // },
+        // {
+        //   name: "View All Roles",
+        //   value: "viewAllRoles",
+        // },
+        // {
+        //   name: "Add Role",
+        //   value: "addRole",
+        // },
+        // {
+        //   name: "Remove Role",
+        //   value: "removeRole",
+        // },
+        // {
+        //   name: "View All Departments",
+        //   value: "viewAllDepartments",
+        // },
+        // {
+        //   name: "Add Department",
+        //   value: "addDepartment",
+        // },
+        // {
+        //   name: "Remove Department",
+        //   value: "removeDepartment",
+        // },
         {
           name: "Quit",
           value: "quit",
@@ -75,4 +75,24 @@ async function mainMenu() {
     },
   ]);
   console.log(choice);
+  switch (choice) {
+    case "viewEmployees":
+      return viewEmployees();
+    case "quit":
+      return quit();
+  }
+}
+
+async function viewEmployees() {
+  const employees = await db.findAllEmployees();
+
+  console.log("\n");
+  console.table(employees);
+
+  mainMenu();
+}
+
+function quit() {
+  console.log("Goodbye!");
+  process.exit();
 }

8396deb05396b898a31cefc3087b6b21b762580b
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-05-17 18:15:07 -0400

    final touches

--- db/connection.js
+++ db/connection.js
@@ -4,14 +4,13 @@ const util = require("util");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "ne14LEELA!",
+  password: "",
   port: "3306",
   database: "employeesdb",
 });
 
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("Connected!");
 });
 
 connection.query = util.promisify(connection.query);
